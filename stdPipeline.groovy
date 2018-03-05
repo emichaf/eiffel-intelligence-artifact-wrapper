d8fa18b223f837c1e9b449bec226e335e3d5b7bd
emichaf <michael.frick@ericsson.com>
2018-03-05 14:37:04 +0100

    m

--- stdPipeline.groovy
+++ stdPipeline.groovy
@@ -2,33 +2,13 @@
 
 def execute() {
 
-    node {
 
-        stage('Initialize') {
-            checkout scm
-            echo 'Loading pipeline definition'
-            //Yaml parser = new Yaml()
-            //Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yml').text)
-            //Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yml').text)
 
             //Map pipelineDefinition = readFile(pwd() + '/pipeline.yml')
             def pipelineDefinition = readYaml file: pwd() + '/pipeline.yml'
 
             println pipelineDefinition
-        }
 
-        switch(pipelineDefinition.pipelineType) {
-            case 'python':
-                // Instantiate and execute a Python pipeline
-                //new pythonPipeline(pipelineDefinition).executePipeline()
-                println "python"
-            case 'nodejs':
-                // Instantiate and execute a NodeJS pipeline
-                //new nodeJSPipeline(pipelineDefinition).executePipeline()
-               println "nodejs"
-        }
-
-    }
 
 
     return this
