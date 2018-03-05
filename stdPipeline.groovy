7f12a88e1b1bc571ea77e34589a1f1808cd5a2a2
emichaf <michael.frick@ericsson.com>
2018-03-05 14:41:33 +0100

    m

--- stdPipeline.groovy
+++ stdPipeline.groovy
@@ -4,15 +4,21 @@ def execute() {
 
     unstash "eiffel-intelligence-artifact-wrapper"
 
-            //Map pipelineDefinition = readFile(pwd() + '/pipeline.yml')
             def pipelineDefinition = readYaml file: pwd() + '/pipeline.yaml'
 
             println pipelineDefinition
 
 
-
-
-
+    switch(pipelineDefinition.pipelineType) {
+        case 'python':
+            // Instantiate and execute a Python pipeline
+            //new pythonPipeline(pipelineDefinition).executePipeline()
+            println "python"
+        case 'nodejs':
+            // Instantiate and execute a NodeJS pipeline
+            //new nodeJSPipeline(pipelineDefinition).executePipeline()
+            println "nodejs"
+    }
 }
 
 return this
