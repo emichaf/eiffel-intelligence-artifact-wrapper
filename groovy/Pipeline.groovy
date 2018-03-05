a850121fb4e83ec136f24dc5491cbe7cb25abc8c
emichaf <michael.frick@ericsson.com>
2018-03-05 15:44:56 +0100

    m

--- groovy/Pipeline.groovy
+++ groovy/Pipeline.groovy
@@ -2,12 +2,11 @@ import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 String DOCKER_HOST = "tcp://docker104-eiffel999.lmera.ericsson.se:4243"
 
 def testar() {
-
+    unstash "eiffel-intelligence-artifact-wrapper"
     try {
 
         docker.withServer("$DOCKER_HOST", 'remote_docker_host') {
             stage('Stage Groovy') {
-                unstash "eiffel-intelligence-artifact-wrapper"
                 println "in stage"
 
                 sh "ls"
